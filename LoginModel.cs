852d67f9fbdd553d31fb4c8c81c2257b30afbb08
MoKhalid99 <aboudy201251@gmail.com>
2025-12-01 23:21:13 +0200

    Add files via upload

---
+++ LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Algorithm_Web.Server.Models
+{
+	public class LoginModel
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ RegisterModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithm_Web.Server.Models
+{
+	public class RegisterModel
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string ConfirmPassword { get; set; } //علشان يتم التأكيد
+
+	}
+}
+
